e0507a31db345f9034dce12b3fa03d092d669d89
saadahmedscse <saadahmedscse@gmail.com>
2023-08-26 16:39:03 +0600

    Initial Commit

---
+++ src/main/java/com/saadahmedsoft/sparkconvo/SparkConvoApplication.java
@@ -0,0 +1,13 @@
+package com.saadahmedsoft.sparkconvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkConvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkConvoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadahmedsoft/sparkconvo/SparkConvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadahmedsoft.sparkconvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkConvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
