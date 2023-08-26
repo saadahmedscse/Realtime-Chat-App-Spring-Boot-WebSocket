569ef1a1644d7429a7b0470571c05823f945ff40
saadahmedscse <saadahmedscse@gmail.com>
2023-08-26 17:35:34 +0600

    created custom web socket handler class

---
+++ src/main/java/com/saadahmedsoft/sparkconvo/config/WebSocketHandler.java
@@ -0,0 +1,32 @@
+package com.saadahmedsoft.sparkconvo.config;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    private final List<WebSocketSession> sessions = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession s : sessions) {
+            s.sendMessage(message);
+            System.out.println(message);
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
