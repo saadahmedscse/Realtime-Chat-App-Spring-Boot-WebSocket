22fa651a225094a7c7380965aae27774c677330a
saadahmedscse <saadahmedscse@gmail.com>
2023-08-28 04:37:29 +0600

    created random string util

---
+++ src/main/java/com/saadahmedsoft/sparkconvo/util/auth/RandomStringUtil.java
@@ -0,0 +1,20 @@
+package com.saadahmedsoft.sparkconvo.util.auth;
+
+import java.util.Random;
+
+public class RandomStringUtil {
+
+    private static final String TEXT = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static String getRandomString(int length) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(26);
+            stringBuilder.append(TEXT.charAt(index));
+        }
+
+        return stringBuilder.toString();
+    }
+}
