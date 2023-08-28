d59ec19772dea18c07dbaabd6823abb013671bdf
saadahmedscse <saadahmedscse@gmail.com>
2023-08-29 01:26:00 +0600

    created getProfile and getAllUsers function

---
+++ src/main/java/com/saadahmedsoft/sparkconvo/controller/user/UserController.java
@@ -0,0 +1,27 @@
+package com.saadahmedsoft.sparkconvo.controller.user;
+
+import com.saadahmedsoft.sparkconvo.service.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<?> getUserExceptMyself(HttpServletRequest request) {
+        return userService.getAllUsersExceptMyself(request);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getProfile(HttpServletRequest request) {
+        return userService.getProfile(request);
+    }
+}
