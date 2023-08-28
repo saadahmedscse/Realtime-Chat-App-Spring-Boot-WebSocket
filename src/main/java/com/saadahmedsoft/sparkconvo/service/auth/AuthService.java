039b771d4d58da60a65aaeda74e5be8d98f32f93
saadahmedscse <saadahmedscse@gmail.com>
2023-08-28 22:34:44 +0600

    added login method

--- src/main/java/com/saadahmedsoft/sparkconvo/service/auth/AuthService.java
+++ src/main/java/com/saadahmedsoft/sparkconvo/service/auth/AuthService.java
@@ -1,6 +1,7 @@
 package com.saadahmedsoft.sparkconvo.service.auth;
 
-import com.saadahmedsoft.sparkconvo.dto.user.UserRequest;
+import com.saadahmedsoft.sparkconvo.dto.auth.CreateAccountRequest;
+import com.saadahmedsoft.sparkconvo.dto.auth.LoginRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,5 +9,6 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public interface AuthService {
 
-    ResponseEntity<?> createAccount(MultipartFile photo, UserRequest userRequest);
+    ResponseEntity<?> createAccount(MultipartFile photo, CreateAccountRequest createAccountRequest);
+    ResponseEntity<?> login(LoginRequest loginRequest);
 }
