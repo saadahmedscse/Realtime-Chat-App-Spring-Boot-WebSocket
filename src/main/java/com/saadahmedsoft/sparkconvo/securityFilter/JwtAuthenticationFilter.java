9286ad2c00c98dacfbcc634f4a000dab607763f1
saadahmedscse <saadahmedscse@gmail.com>
2023-08-28 22:08:14 +0600

    created Jwt authentication filer

---
+++ src/main/java/com/saadahmedsoft/sparkconvo/securityFilter/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.saadahmedsoft.sparkconvo.securityFilter;
+
+import com.saadahmedsoft.sparkconvo.util.auth.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestHeader = request.getHeader("Authorization");
+        String username = null;
+        String token = null;
+
+        if (requestHeader != null && requestHeader.startsWith("Bearer")) {
+            token = requestHeader.substring(7);
+
+            try {
+                username = jwtUtil.getUsernameFromToken(token);
+            } catch (Exception e) {
+                logger.info(e.getLocalizedMessage());
+            }
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            Boolean validateToken = jwtUtil.validateToken(token, userDetails);
+
+            if (validateToken) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+            else {
+                logger.info("Invalid token");
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
