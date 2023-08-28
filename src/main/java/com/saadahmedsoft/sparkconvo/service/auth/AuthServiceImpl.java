309835725be819572758f97f054da1168ca71e1c
saadahmedscse <saadahmedscse@gmail.com>
2023-08-28 22:34:57 +0600

    created login method functionality

--- src/main/java/com/saadahmedsoft/sparkconvo/service/auth/AuthServiceImpl.java
+++ src/main/java/com/saadahmedsoft/sparkconvo/service/auth/AuthServiceImpl.java
@@ -1,14 +1,24 @@
 package com.saadahmedsoft.sparkconvo.service.auth;
 
+import com.saadahmedsoft.sparkconvo.dto.auth.LoginRequest;
+import com.saadahmedsoft.sparkconvo.dto.auth.LoginResponse;
 import com.saadahmedsoft.sparkconvo.dto.common.ApiResponse;
-import com.saadahmedsoft.sparkconvo.dto.user.UserRequest;
+import com.saadahmedsoft.sparkconvo.dto.auth.CreateAccountRequest;
+import com.saadahmedsoft.sparkconvo.entity.auth.Token;
 import com.saadahmedsoft.sparkconvo.entity.user.User;
+import com.saadahmedsoft.sparkconvo.repository.auth.TokenRepository;
 import com.saadahmedsoft.sparkconvo.repository.user.UserRepository;
+import com.saadahmedsoft.sparkconvo.util.auth.JwtUtil;
 import com.saadahmedsoft.sparkconvo.util.common.RandomStringUtil;
 import com.saadahmedsoft.sparkconvo.util.common.ValidatorUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,9 +39,26 @@ public class AuthServiceImpl implements AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private String jwtToken = null;
+
     @Override
-    public ResponseEntity<?> createAccount(MultipartFile photo, UserRequest userRequest) {
-        ResponseEntity<?> validationResult = validatorUtil.isCreateAccountRequestValid(photo, userRequest);
+    public ResponseEntity<?> createAccount(MultipartFile photo, CreateAccountRequest createAccountRequest) {
+        ResponseEntity<?> validationResult = validatorUtil.isCreateAccountRequestValid(photo, createAccountRequest);
         if (validationResult.getStatusCode().equals(HttpStatus.BAD_REQUEST)) return validationResult;
 
         try {
@@ -42,11 +69,11 @@ public class AuthServiceImpl implements AuthService {
             String creationTime = simpleDateFormat.format(new Date());
 
             User user = User.builder()
-                    .name(userRequest.getName())
-                    .email(userRequest.getEmail())
-                    .gender(userRequest.getGender())
+                    .name(createAccountRequest.getName())
+                    .email(createAccountRequest.getEmail())
+                    .gender(createAccountRequest.getGender())
                     .photo(storagePath)
-                    .password(userRequest.getPassword())
+                    .password(passwordEncoder.encode(createAccountRequest.getPassword()))
                     .createdAt(creationTime)
                     .updatedAt(creationTime).build();
 
@@ -57,5 +84,19 @@ public class AuthServiceImpl implements AuthService {
         catch (IOException e) {
             return new ResponseEntity<>(new ApiResponse(false, e.getLocalizedMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @Override
+    public ResponseEntity<?> login(LoginRequest loginRequest) {
+        ResponseEntity<?> validationResult = validatorUtil.isLoginRequestValid(loginRequest);
+        if (validationResult.getStatusCode().isSameCodeAs(HttpStatus.BAD_REQUEST)) return validationResult;
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getEmail());
+        jwtToken = jwtUtil.generateToken(userDetails);
+
+        tokenRepository.save(new Token(jwtToken));
+
+        return new ResponseEntity<>(new LoginResponse(true, "Logged in successfully", jwtToken), HttpStatus.CREATED);
     }
 }
