2e51dd492dd33366c8552ea72ce3a4c2eb2e22b9
saadahmedscse <saadahmedscse@gmail.com>
2023-08-28 22:35:58 +0600

    added login request validation

--- src/main/java/com/saadahmedsoft/sparkconvo/util/common/ValidatorUtil.java
+++ src/main/java/com/saadahmedsoft/sparkconvo/util/common/ValidatorUtil.java
@@ -1,7 +1,8 @@
 package com.saadahmedsoft.sparkconvo.util.common;
 
+import com.saadahmedsoft.sparkconvo.dto.auth.LoginRequest;
 import com.saadahmedsoft.sparkconvo.dto.common.ApiResponse;
-import com.saadahmedsoft.sparkconvo.dto.user.UserRequest;
+import com.saadahmedsoft.sparkconvo.dto.auth.CreateAccountRequest;
 import com.saadahmedsoft.sparkconvo.repository.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,16 +18,25 @@ public class ValidatorUtil {
     @Autowired
     private UserRepository userRepository;
 
-    public ResponseEntity<?> isCreateAccountRequestValid(MultipartFile photo, UserRequest userRequest) {
+    public ResponseEntity<?> isCreateAccountRequestValid(MultipartFile photo, CreateAccountRequest createAccountRequest) {
         if (photo == null || photo.isEmpty()) return generateCommonBadRequestResponse("Profile picture is required");
-        if (userRequest.getName() == null || userRequest.getName().isBlank()) return generateCommonBadRequestResponse("Name is required");
-        if (userRequest.getEmail() == null || userRequest.getEmail().isBlank()) return generateCommonBadRequestResponse("Email is required");
-        if (!userRequest.getEmail().matches(EMAIL_PATTERN)) return generateCommonBadRequestResponse("Invalid email address");
-        if (userRepository.findByEmail(userRequest.getEmail()).isPresent()) return generateCommonBadRequestResponse("Email already exist");
-        if (userRequest.getGender() == null || userRequest.getGender().isBlank()) return generateCommonBadRequestResponse("Gender is required");
-        if (userRequest.getPassword() == null || userRequest.getPassword().isBlank()) return generateCommonBadRequestResponse("Password is required");
-        if (userRequest.getConfirmPassword() == null || userRequest.getConfirmPassword().isBlank()) return generateCommonBadRequestResponse("Confirm password is required");
-        if (!userRequest.getPassword().equals(userRequest.getConfirmPassword())) return generateCommonBadRequestResponse("Password didn't match");
+        if (createAccountRequest.getName() == null || createAccountRequest.getName().isBlank()) return generateCommonBadRequestResponse("Name is required");
+        if (createAccountRequest.getEmail() == null || createAccountRequest.getEmail().isBlank()) return generateCommonBadRequestResponse("Email is required");
+        if (!createAccountRequest.getEmail().matches(EMAIL_PATTERN)) return generateCommonBadRequestResponse("Invalid email address");
+        if (userRepository.findByEmail(createAccountRequest.getEmail()).isPresent()) return generateCommonBadRequestResponse("Email already exist");
+        if (createAccountRequest.getGender() == null || createAccountRequest.getGender().isBlank()) return generateCommonBadRequestResponse("Gender is required");
+        if (createAccountRequest.getPassword() == null || createAccountRequest.getPassword().isBlank()) return generateCommonBadRequestResponse("Password is required");
+        if (createAccountRequest.getConfirmPassword() == null || createAccountRequest.getConfirmPassword().isBlank()) return generateCommonBadRequestResponse("Confirm password is required");
+        if (!createAccountRequest.getPassword().equals(createAccountRequest.getConfirmPassword())) return generateCommonBadRequestResponse("Password didn't match");
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> isLoginRequestValid(LoginRequest loginRequest) {
+        if (loginRequest.getEmail() == null || loginRequest.getEmail().isBlank()) return generateCommonBadRequestResponse("Email is required");
+        if (!loginRequest.getEmail().matches(EMAIL_PATTERN)) return generateCommonBadRequestResponse("Invalid email address");
+        if (userRepository.findByEmail(loginRequest.getEmail()).isEmpty()) return generateCommonBadRequestResponse("Email not found");
+        if (loginRequest.getPassword() == null || loginRequest.getPassword().isBlank()) return generateCommonBadRequestResponse("Password is required");
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
