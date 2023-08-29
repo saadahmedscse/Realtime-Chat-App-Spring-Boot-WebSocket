7be67d14a6eb8a3e98c5b84d3d568577e2765a6c
saadahmedscse <saadahmedscse@gmail.com>
2023-08-30 01:58:44 +0600

    created single conversation request dto

---
+++ src/main/java/com/saadahmedsoft/sparkconvo/dto/chat/SingleConversationRequest.java
@@ -0,0 +1,15 @@
+package com.saadahmedsoft.sparkconvo.dto.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SingleConversationRequest {
+    private String p1Email;
+    private String p2Email;
+}
