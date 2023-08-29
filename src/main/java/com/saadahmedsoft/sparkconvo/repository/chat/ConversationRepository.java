83c18e5683738b5c8112d0514df9ad5a49e6f977
saadahmedscse <saadahmedscse@gmail.com>
2023-08-30 01:58:32 +0600

    created conversation repo and custom query

---
+++ src/main/java/com/saadahmedsoft/sparkconvo/repository/chat/ConversationRepository.java
@@ -0,0 +1,20 @@
+package com.saadahmedsoft.sparkconvo.repository.chat;
+
+import com.saadahmedsoft.sparkconvo.entity.chat.Conversation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ConversationRepository extends JpaRepository<Conversation, Long> {
+
+    @Query("SELECT c FROM Conversation c WHERE c.p1Email LIKE :email OR c.p2Email LIKE :email")
+    List<Conversation> getMyConversations(@Param("email") String email);
+
+    @Query("SELECT c FROM  Conversation  c WHERE c.p1Email like :p1Email AND c.p2Email LIKE :p2Email OR c.p1Email LIKE :p2Email AND c.p2Email LIKE :p1Email")
+    Optional<Conversation> getSinleConversation(@Param("p1Email") String p1Email, @Param("p2Email") String p2Email);
+}
