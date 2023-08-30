798a34dacdcc7d5b4390396c6cf2ad5e3307697b
saadahmedscse <saadahmedscse@gmail.com>
2023-08-31 00:07:00 +0600

    created chat from web socket handler

--- src/main/java/com/saadahmedsoft/sparkconvo/config/WebSocketHandler.java
+++ src/main/java/com/saadahmedsoft/sparkconvo/config/WebSocketHandler.java
@@ -1,5 +1,9 @@
 package com.saadahmedsoft.sparkconvo.config;
 
+import com.google.gson.Gson;
+import com.saadahmedsoft.sparkconvo.dto.chat.ChatRequest;
+import com.saadahmedsoft.sparkconvo.service.chat.ChatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -8,7 +12,10 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.util.ArrayList;
 import java.util.List;
 
+@RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
+
+    private final ChatService chatService;
 
     private final List<WebSocketSession> sessions = new ArrayList<>();
 
@@ -22,6 +29,9 @@ public class WebSocketHandler extends TextWebSocketHandler {
         for (WebSocketSession s : sessions) {
             s.sendMessage(message);
         }
+
+        ChatRequest chatRequest = new Gson().fromJson(message.getPayload(), ChatRequest.class);
+        chatService.createChat(chatRequest);
     }
 
     @Override
